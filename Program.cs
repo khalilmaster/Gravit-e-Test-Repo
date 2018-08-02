76cb76077569287824acdb67e418631998240959
khalilmaster <khalilmaster@hotmail.fr>
2018-08-01 17:00:15 -0700

    1) In either C#, PHP or JavaScript, write a program that prints the numbers from 1 to 75. For multiples of three print “Hello” instead of the number, and for the multiples of five print “World”. For numbers which are multiples of both three and five print “HelloWorld”.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace gravit_e_test
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            for (int i = 1; i <= 75; i++)
+            {
+                string s = (i % 5 == 0) ? "HelloWorld" : "Hello";
+                Console.WriteLine((i % 3 == 0) ? s : ((i % 5 == 0) ? "World" : i.ToString()));
+            }
+
+            Console.WriteLine("Press to any key to terminate de program");
+            Console.ReadKey();
+        }
+    }
+}
